2ab37576129152b4e60e3bd5e613856707a7f9b7
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-27 19:01:58 +0330

    create server

--- server.mjs
+++ server.mjs
@@ -1,34 +1,10 @@
-import { appendFile, readFile, writeFile, unlink } from "fs/promises";
-import path from "path";
-import { URL } from "url";
+import http from "http";
 
-const __dirname = new URL(".", import.meta.url).pathname;
+const PORT = process.env.PORT || 4000;
 
-const fileOps = async () => {
-  try {
-    const data = await readFile(path.join(__dirname, "files", "file1.txt"));
-    console.log(data.toString());
-    await writeFile(
-      path.join(__dirname, "files", "myFile.txt"),
-      data.toString()
-    );
-
-    await appendFile(
-      path.join(__dirname, "files", "myFile.txt"),
-      "\n6666666666666"
-    );
-    const newData = await readFile(path.join(__dirname, "files", "myFile.txt"));
-    console.log(newData.toString());
-
-    await unlink(path.join(__dirname, "files", "file2.txt"));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-fileOps();
-
-process.on("uncaughtException", (err) => {
-  console.log(err);
-  process.exit(1);
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 });
+
+// always at the end
+server.listen(PORT, () => console.log(`server running on ${PORT}`));
