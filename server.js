4762fa9f51777a2bba12df5a26e5a729d976bff1
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-27 14:57:44 +0330

    simple import and export

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-console.log("Hello World");
-console.log(global);
+// console.log("Hello World");
+// console.log(global);
 
-const os = require("os");
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// const os = require("os");
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
 
-const path = require("path");
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
-console.log(path.parse(__filename));
+// const path = require("path");
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.parse(__filename));
+
+const { add } = require("./math");
+console.log(add(1, 3));
