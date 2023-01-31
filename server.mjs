6b8e267902c8fa710bc429391a16bbfd46e0d4de
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-31 09:00:42 +0330

    create web server

--- server.mjs
+++ server.mjs
@@ -1,9 +1,38 @@
 import http from "http";
+import fs from "fs";
 
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+  res.statusCode = 200;
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 // always at the end
